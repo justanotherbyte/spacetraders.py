9e6a002157cf0bc250650768bea4c79d182a3527
quiktea <wishymovies@gmail.com>
2021-05-31 16:57:24 +0100

    Changed properties to attrs in user.py

--- spacetraders/sync/user.py
+++ spacetraders/sync/user.py
@@ -4,26 +4,12 @@ from datetime import datetime
 class LocalUser:
     def __init__(self, data: dict):
         self._data = data
+        self.name = self._data.get("username")
+        self.ship_count = self._data.get("shipCount")
+        self.structure_count = self._data.get("structureCount")
+        self.credits = self._data.get("credits")
     
-    @property
-    def name(self) -> str:
-        return self._data.get("username")
-    
-    @property
-    def ship_count(self) -> int:
-        return self._data.get("shipCount")
-
-    @property
-    def structure_count(self) -> int:
-        return self._data.get("structureCount")
-
     @property
     def joined_at(self) -> datetime:
         return dateparser.parse(self._data.get("joinedAt"))
-    
-    @property
-    def credits(self) -> int:
-        return self._data.get(self._data.get("credits"))
-
-
     
